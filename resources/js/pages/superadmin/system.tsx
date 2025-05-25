90b0a7591f87e44db26dbb836a9706f6d987c315
marahaly <marahaly@gmail.com>
2025-05-26 02:48:55 +0700

    Add some roles

--- resources/js/pages/superadmin/system.tsx
+++ resources/js/pages/superadmin/system.tsx
@@ -2,6 +2,16 @@ import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
